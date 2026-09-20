6e57560ab5894ba0e9bfcd6a32c1d6a4dfb5d19f
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-09-19 20:12:44 -0500

    add: integrity rule solution

--- Server/Homework/Problem3/3a.pl
+++ Server/Homework/Problem3/3a.pl
@@ -3,3 +3,4 @@ age(bob, 10).
 
 
 adult(X) :- age(X, N), N >= 18.
+:- adult(X), grade(X, kindergarten).
--- Server/Homework/Problem3/3b.pl
+++ Server/Homework/Problem3/3b.pl
@@ -4,6 +4,9 @@ age(bob, 10).
 
 adult(X) :- age(X, N), N >= 18.
 
+% forbid adults from being in kindergarten
+:- adult(X), grade(X, kindergarten).
+
 % in addition from 3a.pl
 grade(bob, kindergarten).
-adult(bob).
+adult(bob). % no longer true due to integrity rule
